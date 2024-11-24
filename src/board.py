91f7922a2a01c35e4b1fb9f7737b989b04149a62
zachdenardi <zdenardi@gmail.com>
2024-11-24 14:06:59 -0800

    Moved Rolldice to player class instead of board

--- src/board.py
+++ src/board.py
@@ -1,6 +1,5 @@
 from .player import Player
 from typing import List
-import random
 class Board():
     num_of_spaces: int = 40
     _players: List[Player] = []
@@ -9,17 +8,7 @@ class Board():
         for player in self.players:
             print(str(player.get_pos()))
     
-    def roll_dice(self,player:Player):
-        d1 = random.randint(1,6)
-        d2 = random.randint(1,6)
-        print("Dice 1 is "+str(d1))
-        print("Dice 2 is "+str(d2))
-        if d1 == d2:
-            print("Doubles!")
-        total = d1+d2
-        player.move_player(total)
-        print("Moved Player: "+player.get_name()+" "+str(total)+" spots!")
-        print(player.get_name()+" now at "+str(player.get_pos()))
+
     
     def add_player(self,player:Player):
         player_name = player.get_name()
