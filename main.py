af32c75bfec6af60250b5e267d680dc165221aef
zachdenardi <zdenardi@gmail.com>
2024-11-28 00:20:31 -0800

    Changed roles to rolls

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ board.add_player(p1)
 board.add_player(p2)
 
 
-def playerTurn(player:Player,amount_of_roles:int = 1):
-    if(amount_of_roles ==1):
+def playerTurn(player:Player,amount_of_rolls:int = 1):
+    if(amount_of_rolls == 1):
         print(player.get_name() + " is taking their turn")
     turn = True
     dbls = False
@@ -25,11 +25,11 @@ def playerTurn(player:Player,amount_of_roles:int = 1):
     d2 = dice[1]
     if d1 == d2:
         print("Doubles!")
-        amount_of_roles = amount_of_roles + 1
+        amount_of_rolls = amount_of_rolls + 1
         dbls = True
 
 
-    if(amount_of_roles == 3):
+    if(amount_of_rolls == 3):
         print("Go to Jail")
         turn = False
         pass 
@@ -41,7 +41,7 @@ def playerTurn(player:Player,amount_of_roles:int = 1):
     print(player.get_name()+" now at "+str(space['name']))
     if(dbls):
         print(player.get_name() + " gets to go again!")
-        playerTurn(player,amount_of_roles)
+        playerTurn(player,amount_of_rolls)
     turn = False
 
 playerTurn(p1)
