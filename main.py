9ac41d803fdcb0141ba777909a12042d2c119216
zachdenardi <zdenardi@gmail.com>
2024-12-01 19:23:40 -0800

    Have board created with functions instead of .txt

--- main.py
+++ main.py
@@ -49,14 +49,98 @@ from curses import wrapper,window
 # playerTurn(p2)
        
 def createBoard(stdscr:window):
+    L_INSIDE_BORDER_X = 0
+    R_INSIDE_BORDER_X = 51
+    CORNER_H = 4
+    CORNER_W = 8
+    PROP_LR_H = 2
+    PROP_LR_W = 8
+    PROP_TB_H = 4
+    PROP_TB_W = 5
+
+    BOARD_WIDTH = (CORNER_W*2) + (PROP_TB_W *9)
+    BOARD_HEIGHT = (2 * CORNER_H) + (9 * PROP_LR_H)
+    
+    TOP_BORDER ="_"
+    BOTTOM_BORDER="_"
+    SIDE = "|"
+    TOP_BOTTOM_RAIL = "="
+    RIGHT_SIDE_RAIL = "["
+    LEFT_SIDE_RAIL = "]"
+
     stdscr.clear()
-    boardFile = open('./board.txt')
-    for i,line in enumerate(boardFile):
-        stdscr.addstr(i, 0, line)
+
+    game_board = stdscr.subwin(BOARD_HEIGHT+2,BOARD_WIDTH+2,0,0)
+    for w in range(0,BOARD_WIDTH-3):
+        game_board.addch(0,w,TOP_BORDER)
+        game_board.addch(BOARD_HEIGHT-2,w,BOTTOM_BORDER)
+    for h in range(1,BOARD_HEIGHT-2):
+        game_board.addch(h,0,SIDE)
+        game_board.addch(h,BOARD_WIDTH-4,SIDE)
+    def create_corner(w:window,starting_y,starting_x):
+        corner = w.subwin(CORNER_H,CORNER_W,starting_y,starting_x)
+        for y in range(1,CORNER_H):
+            corner.addch(y,0,SIDE)
+            corner.addch(y,6,SIDE)
+        for x in range(1,6):
+            corner.addch(3,x,BOTTOM_BORDER)
+
+    def create_left_prop(w:window,starting_y,starting_x):
+        prop = w.subwin(PROP_LR_H,PROP_LR_W,starting_y,starting_x)
+        for y in range(0,2):
+            prop.addch(y,0,LEFT_SIDE_RAIL)
+        for x in range(1,6):
+            prop.addch(1,x,BOTTOM_BORDER)
+        
+    def create_right_prop(w:window,starting_y,starting_x):
+        prop = w.subwin(PROP_LR_H,PROP_LR_W,starting_y,starting_x)
+        for y in range(0,2):
+            prop.addch(y,6,RIGHT_SIDE_RAIL)
+        for x in range(1,6):
+            prop.addch(1,x,BOTTOM_BORDER)
+
+    def create_top_prop(w:window,starting_y,starting_x):
+        prop = w.subwin(PROP_TB_H,PROP_TB_W,starting_y,starting_x)
+        # prop.addstr(1,1,"--")
+        for x in range(0,4):
+            prop.addstr(2,x,TOP_BOTTOM_RAIL)
+        for y in range(0,3):
+            prop.addstr(y,4,SIDE)
+     
+
+    def create_bottom_prop(w:window,starting_y,starting_x):
+        prop = w.subwin(PROP_TB_H,PROP_LR_W,starting_y,starting_x)
+        for x in range(0,4):
+            prop.addch(0,x,TOP_BOTTOM_RAIL)
+        for y in range(0,3):
+            prop.addch(y,4,SIDE)
+     
+    
+    
+    free_parking = create_corner(stdscr,0,0)
+    g2j = create_corner(stdscr,0,R_INSIDE_BORDER_X)
+    go = create_corner(stdscr,21,R_INSIDE_BORDER_X)
+    jail = create_corner(stdscr,21,0)
+
+# create left and right side
+    create_left_prop(stdscr,4,R_INSIDE_BORDER_X)
+    create_right_prop(stdscr,4,0)
+
+    for numOfProp in range(2,11):
+        start_y = 4,
+        create_left_prop(stdscr,2*numOfProp,R_INSIDE_BORDER_X)
+        create_right_prop(stdscr,2*numOfProp,L_INSIDE_BORDER_X)
+
+    create_top_prop(stdscr,1,CORNER_W-1)
+    create_bottom_prop(stdscr,22,7)
+    for num in range(3,11):
+        create_top_prop(stdscr,1,PROP_TB_W*num-3)
+        create_bottom_prop(stdscr,22,5*num-3)
+
 
     
+    # game_board.border()
     stdscr.refresh()
-    boardFile.close()
     stdscr.getkey()
 
 
--- src/board.py
+++ src/board.py
@@ -1,7 +1,6 @@
 from .player import Player
 from typing import List
 class Board():
-    num_of_spaces: int = 40
     _players: List[Player] = []
 
     def get_players_pos(self):
@@ -13,4 +12,40 @@ class Board():
         self._players.append(player)
         print(player_name+" has been added")
 
+        
+
+# corner is 6 long
+# propertyTB is 4 long
+# property LR is 6 long
+# each side has two corners and 9 properties 
+# each side has 12 sides/walls. WccccccWccccW 
+# 12+36+12 "characters" = 60
+# (y,x)
+
+# Top border = 60 '_' (0,0 -> 0,60)
+# Outside border R '|' (1,1 -> 59,1)
+# Bottom Border '‾' (1,60 -> 60->60)
+# Outside border L '|' (1,59->59,59)
+
+# FP - 1,1 -> 6,6
+
+    def create_corner(self):
+        top_bottom = " "*6
+        second_line = " TEST "
+
+    def create_top_property(self):
+        top_line = " "* 4
+        second_line = " ?? "
+        third_line = "="*4
+
+    def create_bottom_property(self):
+        top_line = " "* 4
+        second_line = " ?? "
+        third_line = " "*4
     
+    def create_left_right_property(self):
+        top_line = " ?? "
+        bottom_line = " "*6
+
+
+
