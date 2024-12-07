0b6e45df4553d14a4997105883f9c4f215689e51
zachdenardi <zdenardi@gmail.com>
2024-12-07 00:22:54 -0800

    added ui window

--- main.py
+++ main.py
@@ -2,53 +2,74 @@ from src.board import Board
 from src.player import Player
 from src.property import Property
 import json
-from curses import wrapper
+from curses import wrapper, window
 import curses
 
 
+def startGame():
+    stdscr = curses.initscr()
+    board = Board()
 
-# print("Starting")
-# p1 = Player("Zach")
-# p2 = Player("Sarah")
+    board_width = board.get_board_width()
+    board_height = board.get_board_height()
 
-# board = Board()
-# board.add_player(p1)
-# board.add_player(p2)
+    UI_WIDTH = int(curses.COLS - board_width)
+
+    board.createBoard(stdscr)
+    ui_win = curses.newwin(curses.LINES, UI_WIDTH, 0, board_width)
+    ui_win.border()
+
+    line_height = 1
+
+    p1 = Player("Zach")
+    p2 = Player("Sarah")
+
+    board = Board()
+    board.add_player(p1)
+    board.add_player(p2)
+    line_height = 1
+
+    def print_ui_window(text: str, line_height: int):
+        ui_win.addstr(line_height, 2, text + "/n")
+        line_height = line_height + 1
+
+    print_ui_window("Hello", line_height)
+    print_ui_window("Two", line_height)
+
+    # def playerTurn(player:Player,amount_of_rolls:int = 1):
+    #     if(amount_of_rolls == 1):
+    #         print(player.get_name() + " is taking their turn")
+    #     turn = True
+    #     dbls = False
+    #     dice = player.roll_dice()
+    #     d1 = dice[0]
+    #     d2 = dice[1]
+    #     if d1 == d2:
+    #         print("Doubles!")
+    #         amount_of_rolls = amount_of_rolls + 1
+    #         dbls = True
+
+    #     if(amount_of_rolls == 3):
+    #         print("Go to Jail")
+    #         turn = False
+    #         pass
+
+    #     total = d1+d2
+    #     player.move_player(total)
+    #     print("Moved Player: "+player.get_name()+" "+str(total)+" spots!")
+    #     space = data[player.get_pos()]
+    #     print(player.get_name()+" now at "+str(space['name']))
+    #     if(dbls):
+    #         print(player.get_name() + " gets to go again!")
+    #         playerTurn(player,amount_of_rolls)
+    #     turn = False
+
+    # playerTurn(p1)
+    # playerTurn(p2)
+
+    stdscr.refresh()
+    ui_win.refresh()
+    stdscr.getkey()
 
 
-# def playerTurn(player:Player,amount_of_rolls:int = 1):
-#     if(amount_of_rolls == 1):
-#         print(player.get_name() + " is taking their turn")
-#     turn = True
-#     dbls = False
-#     dice = player.roll_dice()
-#     d1 = dice[0]
-#     d2 = dice[1]
-#     if d1 == d2:
-#         print("Doubles!")
-#         amount_of_rolls = amount_of_rolls + 1
-#         dbls = True
-
-
-#     if(amount_of_rolls == 3):
-#         print("Go to Jail")
-#         turn = False
-#         pass
-
-#     total = d1+d2
-#     player.move_player(total)
-#     print("Moved Player: "+player.get_name()+" "+str(total)+" spots!")
-#     space = data[player.get_pos()]
-#     print(player.get_name()+" now at "+str(space['name']))
-#     if(dbls):
-#         print(player.get_name() + " gets to go again!")
-#         playerTurn(player,amount_of_rolls)
-#     turn = False
-
-# playerTurn(p1)
-# playerTurn(p2)
-
-
-stdscr = curses.initscr()
-
-wrapper(Board.createBoard(stdscr))
+wrapper(startGame())
--- src/board.py
+++ src/board.py
@@ -6,6 +6,17 @@ from curses import wrapper, window
 
 class Board:
     _players: List[Player] = []
+    _L_INSIDE_BORDER_X: int = 0
+    _R_INSIDE_BORDER_X: int = 51
+    _CORNER_H: int = 4
+    _CORNER_W: int = 8
+    _PROP_LR_H: int = 2
+    _PROP_LR_W: int = 8
+    _PROP_TB_H: int = 4
+    _PROP_TB_W: int = 5
+
+    _BOARD_WIDTH: int = (_CORNER_W * 2) + (_PROP_TB_W * 9)
+    _BOARD_HEIGHT: int = (2 * _CORNER_H) + (9 * _PROP_LR_H)
 
     def get_players_pos(self):
         for player in self.players:
@@ -16,7 +27,7 @@ class Board:
         self._players.append(player)
         print(player_name + " has been added")
 
-    def createBoard(stdscr: window):
+    def createBoard(self, stdscr: window):
         # Get property JSON
         file = open("src/spaces.json")
         data = json.load(file)
@@ -31,18 +42,6 @@ class Board:
         BOTTOM_SPACES.reverse()  # must be reversed for logic of render
         LEFT_SPACES.reverse()  # must be reversed for logic of render
 
-        L_INSIDE_BORDER_X = 0
-        R_INSIDE_BORDER_X = 51
-        CORNER_H = 4
-        CORNER_W = 8
-        PROP_LR_H = 2
-        PROP_LR_W = 8
-        PROP_TB_H = 4
-        PROP_TB_W = 5
-
-        BOARD_WIDTH = (CORNER_W * 2) + (PROP_TB_W * 9)
-        BOARD_HEIGHT = (2 * CORNER_H) + (9 * PROP_LR_H)
-
         TOP_BORDER = "_"
         BOTTOM_BORDER = "_"
         SIDE = "|"
@@ -52,21 +51,21 @@ class Board:
 
         stdscr.clear()
 
-        game_board = stdscr.subwin(BOARD_HEIGHT + 2, BOARD_WIDTH + 2, 0, 0)
-        for w in range(0, BOARD_WIDTH - 3):
+        game_board = stdscr.subwin(self._BOARD_HEIGHT + 2, self._BOARD_WIDTH + 2, 0, 0)
+        for w in range(0, self._BOARD_WIDTH - 3):
             game_board.addch(0, w, TOP_BORDER)
-            game_board.addch(BOARD_HEIGHT - 2, w, BOTTOM_BORDER)
-        for h in range(1, BOARD_HEIGHT - 2):
+            game_board.addch(self._BOARD_HEIGHT - 2, w, BOTTOM_BORDER)
+        for h in range(1, self._BOARD_HEIGHT - 2):
             game_board.addch(h, 0, SIDE)
-            game_board.addch(h, BOARD_WIDTH - 4, SIDE)
+            game_board.addch(h, self._BOARD_WIDTH - 4, SIDE)
 
         # Creates corner spaces
         def create_corner(
             w: window, starting_y: int, starting_x: int, corner_name: str = "--"
         ):
-            corner = w.subwin(CORNER_H, CORNER_W, starting_y, starting_x)
+            corner = w.subwin(self._CORNER_H, self._CORNER_W, starting_y, starting_x)
             corner.addstr(2, 2, corner_name)
-            for y in range(1, CORNER_H):
+            for y in range(1, self._CORNER_H):
                 corner.addch(y, 0, SIDE)
                 corner.addch(y, 6, SIDE)
             for x in range(1, 6):
@@ -76,7 +75,7 @@ class Board:
         def create_right_prop(
             w: window, starting_y: int, starting_x: int, prop_abr: str = "--"
         ):
-            prop = w.subwin(PROP_LR_H, PROP_LR_W, starting_y, starting_x)
+            prop = w.subwin(self._PROP_LR_H, self._PROP_LR_W, starting_y, starting_x)
             prop.addstr(0, 2, prop_abr)
             for y in range(0, 2):
                 prop.addch(y, 0, LEFT_SIDE_RAIL)
@@ -87,7 +86,7 @@ class Board:
         def create_left_prop(
             w: window, starting_y: int, starting_x: int, prop_abr: str = "--"
         ):
-            prop = w.subwin(PROP_LR_H, PROP_LR_W, starting_y, starting_x)
+            prop = w.subwin(self._PROP_LR_H, self._PROP_LR_W, starting_y, starting_x)
             prop.addstr(0, 2, prop_abr)
 
             for y in range(0, 2):
@@ -99,7 +98,7 @@ class Board:
         def create_top_prop(
             w: window, starting_y: int, starting_x: int, prop_abr: str = "--"
         ):
-            prop = w.subwin(PROP_TB_H, PROP_TB_W, starting_y, starting_x)
+            prop = w.subwin(self._PROP_TB_H, self._PROP_TB_W, starting_y, starting_x)
             prop.addstr(1, 1, prop_abr)
             for x in range(0, 4):
                 prop.addstr(2, x, TOP_BOTTOM_RAIL)
@@ -110,7 +109,7 @@ class Board:
         def create_bottom_prop(
             w: window, starting_y: int, starting_x: int, prop_abr: str = "--"
         ):
-            prop = w.subwin(PROP_TB_H, PROP_LR_W, starting_y, starting_x)
+            prop = w.subwin(self._PROP_TB_H, self._PROP_LR_W, starting_y, starting_x)
             prop.addstr(1, 1, prop_abr)
             for x in range(0, 4):
                 prop.addch(0, x, TOP_BOTTOM_RAIL)
@@ -130,30 +129,35 @@ class Board:
 
         # create corners
         free_parking = create_corner(stdscr, 0, 0, "0-0")
-        g2j = create_corner(stdscr, 0, R_INSIDE_BORDER_X, "!!!")
-        go = create_corner(stdscr, 21, R_INSIDE_BORDER_X, "GO!")
+        g2j = create_corner(stdscr, 0, self._R_INSIDE_BORDER_X, "!!!")
+        go = create_corner(stdscr, 21, self._R_INSIDE_BORDER_X, "GO!")
         jail = create_corner(stdscr, 21, 0, "|||")
 
         # create left and right side
-        create_right_prop(stdscr, 4, R_INSIDE_BORDER_X)
+        create_right_prop(stdscr, 4, self._R_INSIDE_BORDER_X)
         create_left_prop(stdscr, 4, 0)
 
         for i in range(2, 11):
             left_abr = get_property_abbr(LEFT_SPACES[i - 2]["name"])
             right_abr = get_property_abbr(RIGHT_SPACES[i - 2]["name"])
-            create_left_prop(stdscr, 2 * i, L_INSIDE_BORDER_X, left_abr)
+            create_left_prop(stdscr, 2 * i, self._L_INSIDE_BORDER_X, left_abr)
 
-            create_right_prop(stdscr, 2 * i, R_INSIDE_BORDER_X, right_abr)
+            create_right_prop(stdscr, 2 * i, self._R_INSIDE_BORDER_X, right_abr)
 
         # create top and bottom
-        spacer = CORNER_W - 1
+        spacer = self._CORNER_W - 1
 
         for i in range(0, 9):
             top_abbr = get_property_abbr(TOP_ROW_SPACES[i]["name"])
             bottom_abbr = get_property_abbr(BOTTOM_SPACES[i]["name"])
             create_top_prop(stdscr, 1, spacer, top_abbr)
             create_bottom_prop(stdscr, 22, spacer, bottom_abbr)
-            spacer = spacer + PROP_TB_W
+            spacer = spacer + self._PROP_TB_W
 
-        stdscr.refresh()
-        stdscr.getkey()
+        return stdscr
+
+    def get_board_width(self):
+        return self._BOARD_WIDTH
+
+    def get_board_height(self):
+        return self._BOARD_HEIGHT
