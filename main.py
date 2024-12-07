64394e9406d0d49b1844e9fa35b8a439d74cd5b6
zachdenardi <zdenardi@gmail.com>
2024-12-07 00:42:31 -0800

    Have text helper function for ui_win

--- main.py
+++ main.py
@@ -16,10 +16,17 @@ def startGame():
     UI_WIDTH = int(curses.COLS - board_width)
 
     board.createBoard(stdscr)
-    ui_win = curses.newwin(curses.LINES, UI_WIDTH, 0, board_width)
-    ui_win.border()
 
-    line_height = 1
+    file = open("src/spaces.json")
+    data = json.load(file)
+    file.close()
+
+    ui_win = curses.newwin(curses.LINES, UI_WIDTH, 0, board_width)
+    # Creates the nice border
+    ui_win.border()
+    # This is creating text subwindow inside the UI_WIN
+    ui_text = ui_win.subwin(curses.LINES-2,UI_WIDTH-4,2,board_width+2)
+
 
     p1 = Player("Zach")
     p2 = Player("Sarah")
@@ -27,45 +34,44 @@ def startGame():
     board = Board()
     board.add_player(p1)
     board.add_player(p2)
-    line_height = 1
 
-    def print_ui_window(text: str, line_height: int):
-        ui_win.addstr(line_height, 2, text + "/n")
-        line_height = line_height + 1
+    def print_ui_text(text:str):
+        ui_text.addstr(text + "\n")
+    
+    print_ui_text("Starting...")
 
-    print_ui_window("Hello", line_height)
-    print_ui_window("Two", line_height)
 
-    # def playerTurn(player:Player,amount_of_rolls:int = 1):
-    #     if(amount_of_rolls == 1):
-    #         print(player.get_name() + " is taking their turn")
-    #     turn = True
-    #     dbls = False
-    #     dice = player.roll_dice()
-    #     d1 = dice[0]
-    #     d2 = dice[1]
-    #     if d1 == d2:
-    #         print("Doubles!")
-    #         amount_of_rolls = amount_of_rolls + 1
-    #         dbls = True
 
-    #     if(amount_of_rolls == 3):
-    #         print("Go to Jail")
-    #         turn = False
-    #         pass
+    def playerTurn(player:Player,amount_of_rolls:int = 1):
+        if(amount_of_rolls == 1):
+            print_ui_text(player.get_name() + " is taking their turn...")
+        turn = True
+        dbls = False
+        dice = player.roll_dice()
+        d1 = dice[0]
+        d2 = dice[1]
+        if d1 == d2:
+            print_ui_text("Doubles!")
+            amount_of_rolls = amount_of_rolls + 1
+            dbls = True
 
-    #     total = d1+d2
-    #     player.move_player(total)
-    #     print("Moved Player: "+player.get_name()+" "+str(total)+" spots!")
-    #     space = data[player.get_pos()]
-    #     print(player.get_name()+" now at "+str(space['name']))
-    #     if(dbls):
-    #         print(player.get_name() + " gets to go again!")
-    #         playerTurn(player,amount_of_rolls)
-    #     turn = False
+        if(amount_of_rolls == 3):
+            print_ui_text("Go to Jail")
+            turn = False
+            pass
 
-    # playerTurn(p1)
-    # playerTurn(p2)
+        total = d1+d2
+        player.move_player(total)
+        print_ui_text("Moved Player: "+player.get_name()+" "+str(total)+" spots!")
+        space = data[player.get_pos()]
+        print_ui_text(player.get_name()+" now at "+str(space['name']))
+        if(dbls):
+            print_ui_text(player.get_name() + " gets to go again!")
+            playerTurn(player,amount_of_rolls)
+        turn = False
+
+    playerTurn(p1)
+    playerTurn(p2)
 
     stdscr.refresh()
     ui_win.refresh()
