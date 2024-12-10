730d9e7bd6958bcb8122813bbd3f74e3dbac1dfc
zachdenardi <zdenardi@gmail.com>
2024-12-10 00:29:48 -0800

    Got text box working, but now print is printing in the wrong window

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from src.board import Board
 from src.player import Player
 from src.property import Property
 import json
-from curses import wrapper, window
 import curses
 
+from curses import wrapper, window
+from curses.textpad import Textbox, rectangle
 
-def startGame():
-    stdscr = curses.initscr()
+
+
+def startGame(stdscr:window):
     board = Board()
+    WAITING = False
 
     board_width = board.get_board_width()
     board_height = board.get_board_height()
@@ -21,27 +24,18 @@ def startGame():
     data = json.load(file)
     file.close()
 
-    ui_win = curses.newwin(curses.LINES, UI_WIDTH, 0, board_width)
     # Creates the nice border
-    ui_win.border()
-    # This is creating text subwindow inside the UI_WIN
-    ui_text = ui_win.subwin(curses.LINES-2,UI_WIDTH-4,2,board_width+2)
+    ui_text = curses.newwin(int(curses.LINES/2),40,2,board_width+2)
+    rectangle(stdscr,1,board_width+1,25,board_width*2)
 
-
-    p1 = Player("Zach")
-    p2 = Player("Sarah")
-
-    board = Board()
-    board.add_player(p1)
-    board.add_player(p2)
+    player_win = curses.newwin(board_height-8,board_width-15,5,9)
+    rectangle(stdscr,4,8,9,board_width-16)
+    player_win.refresh()
 
     def print_ui_text(text:str):
         ui_text.addstr(text + "\n")
     
     print_ui_text("Starting...")
-
-
-
     def playerTurn(player:Player,amount_of_rolls:int = 1):
         if(amount_of_rolls == 1):
             print_ui_text(player.get_name() + " is taking their turn...")
@@ -70,12 +64,36 @@ def startGame():
             playerTurn(player,amount_of_rolls)
         turn = False
 
+    print_ui_text( "Enter Player 1's name and press CTRL+G ")
+    
+    box = Textbox(player_win)
+
+    stdscr.refresh()
+    ui_text.refresh()
+    player_win.refresh()
+    WAITING = True
+
+    while WAITING:
+        box.edit()
+        player_win.clear()
+        stdscr.refresh()
+        ui_text.refresh()
+        player_win.refresh()
+        WAITING = False
+    
+    player_name = box.gather().strip()
+
+    p1 = Player(player_name)
+    p2 = Player("Sarah")
+
+    board.add_player(p1)
+    board.add_player(p2)
+
     playerTurn(p1)
     playerTurn(p2)
 
-    stdscr.refresh()
-    ui_win.refresh()
     stdscr.getkey()
 
 
-wrapper(startGame())
+if __name__ == "__main__":
+    wrapper(startGame)
